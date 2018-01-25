ecbeed5f4c01f7105e57f75c37605f5e57dcf037
unknown <jhumphrey321@gmail.com>
2018-01-25 13:41:35 -0500

    Got P3 to a submitable (yet incorrect) state

--- p3/p3.m
+++ p3/p3.m
@@ -1,27 +1,31 @@
 % System Simulation Problem 3
 % Written by Josh Humphrey
 
-
-t = linspace(0,5,100);
-s = (1./t).*log(z);
+z = linspace(0.5,100);
+deltaT = 0.01;
+s = (1./deltaT).*log(z);
 Hz_approx = (50*(-s.^2+33.3333*s+13333.3))./(s.^3+185.333*s.^2+12133.3*s+693333);
 
 for t = 0:5
+    figure(1)
     plot(Hz_approx*unitStep(t))
     title('Step Response')
     xlim([0;30])
-    xlabel('z-value')
+    xlabel('time (seconds)')
     ylabel('Step Response Magnitude')
 end
 
-t = linspace(-5,5,100);
-s = (1./t).*log(z);
+z = linspace(-5,5,200);
+s = (1./deltaT).*log(z);
 Hz_approx = (50*(-s.^2+33.3333*s+13333.3))./(s.^3+185.333*s.^2+12133.3*s+693333);
 
+
 for t = -5:5
+    figure(2)
     plot(Hz_approx)
     title('Impulse Response')
-    xlim([-5;5])
-    xlabel('z-value')
+    xlim([-1;1])
+    ylim([-0.1,0.3])
+    xlabel('time (seconds)')
     ylabel('Impulse Response Magnitude')
 end
