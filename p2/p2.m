ddba93b1a7e1b22e1c8df2ade2205857ad13be09
unknown <jhumphrey321@gmail.com>
2018-01-28 15:14:39 -0500

    2 is bad

--- p2/p2.m
+++ p2/p2.m
@@ -1,22 +1,26 @@
 % System Simulation Problem 2
 % Written by Josh Humphrey
 
+%% Part A
 % Assume a ZOH function that lasts for one half second
-zoh = zeros(1000);
-for count = 1:1000;
-    if count < 15
-        zoh(count) = 0.5;
+zoh = zeros(1,1000);
+for count = 1:100
+    if count < 50
+        if count < 15
+            zoh(1,count) = 0.5;
+        end
+        if count >= 15 && count < 35
+            zoh(1,count) = 1;
+        end
+        if count >= 35 && count < 50
+            zoh(1, count) = 0.5;
+        end
+    else
+        zoh(1,count) = 0;
     end
-    if count > 15 && count < 35
-        zoh(count) = 1;
-    end
-    if count >= 35 && count < 50
-        zoh(count) = 0.5;
-    end
+    
 end
     
-
-%% Part A
 ykm4 = 0;
 ykm3 = 0;
 ykm2 = 0;
@@ -25,8 +29,8 @@ ekm3 = 0;
 ekm2 = 0;
 ekm1 = 0;
 ek = zoh;
-hold on
-for i = 1:1000
+
+for i = 1:100
     yk = (77.376*ekm2+189.904*ekm1+16.16*ek(i)-ykm4-14*ykm3-99.862*ykm1)*(1/285.32);
     ykm4 = ykm3;
     ykm3 = ykm2;
@@ -35,9 +39,93 @@ for i = 1:1000
     ekm3 = ekm2;
     ekm2 = ekm1;
     ekm1 = ek;
+end
+
+    figure(1)
+    plot(yk)
+    title('Part A')
+    xlim([0,100])
+    xlabel('Time in Milliseconds')
+    ylabel('Y(k): Response Magnitude')
     
-    plot(yk, 'o')
+%% Part B
+stepVector = ones(1,100);
+ykm4 = 0;
+ykm3 = 0;
+ykm2 = 0;
+ykm1 = 0;
+ekm3 = 0;
+ekm2 = 0;
+ekm1 = 0;
+ek = stepVector;
+
+for i = 1:100
+    yk = (77.376*ekm2+189.904*ekm1+16.16*ek(i)-ykm4-14*ykm3-99.862*ykm1)*(1/285.32);
+    ykm4 = ykm3;
+    ykm3 = ykm2;
+    ykm2 = ykm1;
+    ykm1 = yk;
+    ekm3 = ekm2;
+    ekm2 = ekm1;
+    ekm1 = ek;
+    ek = stepVector-ykm1;
+end
+
+    figure(2)
+    plot(yk)
+    title('Part B')
+    xlim([0,100])
+    xlabel('Time in Milliseconds')
+    ylabel('E(k): Response Magnitude')
+
+%% Part C
+toh = zeros(1,1000);
+i = 1;
+for count = 1:100
+    if count < 50
+        if count < 25
+            toh(1, count) = count/25;
+        end
+        if count > 25
+
+            toh(1, count) = toh(25-i);
+            i = 1 + i;
+        end
+        if count == 25
+            toh(1, count) = 1;
+        end
+    else
+        toh(1,count) = 0;
+    end
     
 end
-hold off
+    
+ykm4 = 0;
+ykm3 = 0;
+ykm2 = 0;
+ykm1 = 0;
+ekm3 = 0;
+ekm2 = 0;
+ekm1 = 0;
+ek = toh;
 
+for i = 1:100
+    yk = (77.376*ekm2+189.904*ekm1+16.16*ek(i)-ykm4-14*ykm3-99.862*ykm1)*(1/285.32);
+    ykm4 = ykm3;
+    ykm3 = ykm2;
+    ykm2 = ykm1;
+    ykm1 = yk;
+    ekm3 = ekm2;
+    ekm2 = ekm1;
+    ekm1 = ek;
+end
+
+    figure(3)
+    plot(yk)
+    title('Part C')
+    xlim([0,100])
+    xlabel('Time in Milliseconds')
+    ylabel('Y(k): Response Magnitude')
+    
+%% Part D
+% No clue
