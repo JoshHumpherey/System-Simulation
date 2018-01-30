5ba94665c7bd421e740fdd51dccda8e909ba85ba
unknown <jhumphrey321@gmail.com>
2018-01-30 12:38:39 -0500

    Finished P3

--- p3/p3.m
+++ p3/p3.m
@@ -1,44 +1,31 @@
 % System Simulation Problem 3
 % Written by Josh Humphrey
 
+z = linspace(1,100);
+T = 0.01
 unitStep = ones(1,100);
-impulse = zeros(1,100);
-impulse(1,1) = 1;
-stepResults = zeros(1,100);
-impulseResults = zeros(1,100);
-wk = unitStep;
-xkm3 = 0;
-xkm2 = 0;
-xkm1 = 0;
-wkm2 = 0;
-wkm1 = 0;
-for k = 1:100
-    stepResults(1,k) = (1/693333)*(-xkm3-185.333*xkm2-12133.3*xkm1+50*(-wkm2+33.3333*wkm1+133333*wk(k)));
-    xkm3 = xkm2;
-    xkm2 = xkm1;
-    xkm1 = stepResults(1,k);
-    wkm2 = wkm1;
-    wkm1 = wk(k);
+unitImpulse = zeros(1,100);
+for i = 1:5
+    unitImpulse(1,i) = 1;
+end
+unitStepResults = zeros(1,100);
+unitImpulseResults = zeros(1,100);
+
+Gdiscrete = @(z)(50*(-1+33.3333.*(T./(z-1))+13333.33.*(((T.^2)*(z))./((z-1).^2))))./(1+185.333.*(T./(z-1))+12133.3.*(((T.^2)*(z))./((z-1).^2))+693333.*(((T.^3)*(z^2+z))./(2*(z-1).^3)));
+
+for z = 1:100
+    unitStepResults(1,z) = Gdiscrete(z)*unitStep(1,z);
+    unitImpulseResults(1,z) = Gdiscrete(z)*unitImpulse(1,z);
 end
 
-wk = impulse;
-xkm3 = 0;
-xkm2 = 0;
-xkm1 = 0;
-wkm2 = 0;
-wkm1 = 0;
-for k = 1:100
-    impulseResults(1,k) = (1/693333)*(-xkm3-185.333*xkm2-12133.3*xkm1+50*(-wkm2+33.3333*wkm1+133333*wk(k)));
-    xkm3 = xkm2;
-    xkm2 = xkm1;
-    xkm1 = impulseResults(1,k);
-    wkm2 = wkm1;
-    wkm1 = wk(k);
-end
-hold on
-plot(impulseResults)
-plot(stepResults)
-title('Responses from Unit Step and Impulse')
-legend('Impulse Response','Step Response')
-xlabel('k')
+figure(1)
+plot(unitStepResults)
+title('Unit Step Response')
+xlabel('Z')
+ylabel('Response Magnitude')
+
+figure(2)
+plot(unitImpulseResults)
+title('Unit Impulse Response')
+xlabel('Z')
 ylabel('Response Magnitude')
