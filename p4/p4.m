1b0f28457d7b5f73eddb14ce437fb24e4d0db5f1
unknown <jhumphrey321@gmail.com>
2018-02-06 11:45:27 -0500

    Finished P4

--- p4/p4.m
+++ p4/p4.m
@@ -7,16 +7,24 @@ T = 0.01;
 z1 = ones(1,N+1);
 z2 = ones(1,N+1);
 s = ((z-1)/(T*z));
-C1 = @(z) (-2/(1+s))*z1(1,z)+(3/(1+s))*z2(1,z);
-C2 = @(z) (4/(1+s))*z1(1,z)+((2+8*s)/(4/(s+1)))*z2(1,z);
+C1 = @(t) (3*100.^(t)*101.^(-t-1))*z1(1,t)+(-2.^(2*t+1)*25.^(t)*101.^(-t-1))*z2(1,t);
+C2 = @(t) (4.^(t+1)*25.^(t)*101.^(-t-1))*z1(1,t)+(7.94059*heaviside(t)-3*2.^(2*t+1)*25.^(t)*101.^(-t-1)*1-heaviside(-t))*z2(1,t);
 results1 = zeros(1,N+1);
 results2 = zeros(1,N+1);
 
-for z = 1:(N+1)
-    results1(1,z) = C1(z);
-    results2(1,z) = C2(z);
+for t = 1:(N+1)
+    results1(1,t) = C1(t);
+    results2(1,t) = C2(t);
 end
 
 figure(1)
-plot(results1,'o')
-plot(results2,'o')
+plot(results1)
+title('C1')
+xlabel('time')
+ylabel('magnitude')
+
+figure(2)
+plot(results2)
+title('C2')
+xlabel('time')
+ylabel('magnitude')
