b0f8e0984a979c4be4d431446d6326411c686322
JoshHumpherey <jhumphrey321@gmail.com>
2018-02-19 15:34:07 -0500

    Have correct equations for P6

--- p6/p6.m
+++ p6/p6.m
@@ -1,11 +1,11 @@
 % Problem 6 System Simulation Homework
 % Written by Josh Humphrey
 T = 0.01;
-N = 100;
+N = 10000001;
 
 a = 0.2;
 b = 0.2;
-c = 5.7;
+c = 4;
 x(1) = 5;
 y(1) = 5;
 z(1) = 10;
@@ -17,62 +17,17 @@ x = linspace(0,period,N);
 y = linspace(0,period,N);
 z = linspace(0,period,N);
 
-for t = 1:N-1
-   x(t+1) = (-y(t)-z(t));
-   y(t+1) = x(t)+a*y(t);
-   z(t+1) = b+z(t)*(x(t)-c);
+for t = 2:N-1
+   fx(t) = (-y(t)-z(t));
+   fy(t) = x(t)+a*y(t);
+   fz(t) = b+z(t)*(x(t)-c);
+   
+   x(t+1) = x(t) + (3*T)./(2)*fx(t)-(T/2)*fx(t-1);
+   y(t+1) = y(t) + (3*T)./(2)*fy(t)-(T/2)*fy(t-1);
+   z(t+1) = z(t) + (3*T)./(2)*fz(t)-(T/2)*fz(t-1);
+
 end
 figure(1)
-plot(x,y)
+plot(x(5000000:N),y(5000000:N))
 title('x vs. y with a period of 1')
 
-% Part A2
-period = 2;
-t = linspace(0,period,N);
-x = linspace(0,period,N);
-y = linspace(0,period,N);
-z = linspace(0,period,N);
-
-for t = 1:N-1
-   x(t+1) = (-y(t)-z(t));
-   y(t+1) = x(t)+a*y(t);
-   z(t+1) = b+z(t)*(x(t)-c);
-end
-figure(2)
-plot(x,y)
-title('x vs. y with a period of 2')
-
-% Part A3
-period = 3;
-t = linspace(0,period,N);
-x = linspace(0,period,N);
-y = linspace(0,period,N);
-z = linspace(0,period,N);
-
-for t = 1:N-1
-   x(t+1) = (-y(t)-z(t));
-   y(t+1) = x(t)+a*y(t);
-   z(t+1) = b+z(t)*(x(t)-c);
-end
-figure(3)
-plot(x,y)
-title('x vs. y with a period of 3')
-
-% Part B
-a = 0.3;
-b = 0.3;
-c = 8.0;
-
-period = 3;
-t = linspace(0,period,N);
-x = linspace(0,period,N);
-y = linspace(0,period,N);
-z = linspace(0,period,N);
-
-for t = 1:N-1
-   x(t+1) = (-y(t)-z(t));
-   y(t+1) = x(t)+a*y(t);
-   z(t+1) = b+z(t)*(x(t)-c);
-end
-plot3(x,y,z)
-title('3D plot of X,Y,Z')
