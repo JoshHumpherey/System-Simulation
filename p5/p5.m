9c49cbc151ef606d89bcc39069093c9dceb419f9
unknown <jhumphrey321@gmail.com>
2018-02-26 20:00:18 -0500

    Finished up problem 8

--- p5/p5.m
+++ p5/p5.m
@@ -1,7 +1,7 @@
 % System Simulation Problem 5
 % Written by Josh Humphrey
-N = 100;
-T = 0.1;
+N = 10000;
+T = 0.01;
 t = linspace(0,10,N+1);
 u1 = ones(1,N);
 u2 = ones(1,N);
@@ -18,17 +18,17 @@ y1 = x1;
 y2 = x1;
 
 for k = 1:N
-    f1(k) = -x1(k)-2*u1(1,k);
-    x1(k+1)=-x1(k)-2*u1(k)+T*f1(k);
+    f1(k) = -x1(k)-2*u1(k);
+    x1(k+1)=x1(k)+T*f1(k);
     
-    f2(k) = -x2(k)+4*u1(1,k);
-    x2(k+1) = -x2(k)+4*u1(1,k)+T*f2(k);
+    f2(k) = -x2(k)+4*u1(k);
+    x2(k+1) =x2(k)+T*f2(k);
     
     f3(k) = -x3(k)+3*u2(1,k);
-    x3(k+1) = -x3(k)+3*u2(1,k)+T*f3(k);
+    x3(k+1) = x2(k)+T*f3(k);
     
     f4(k) = -x4(k)-6*u2(1,k);
-    x4(k+1) = -x4(k)-6*u2(1,k)+T*f4(k);
+    x4(k+1) = x4(k)+T*f4(k);
     
     y1(k+1) = x1(k+1)+x3(k+1);
     y2(k+1) = x2(k+1)+x4(k+1)+8*u2(1,k);
