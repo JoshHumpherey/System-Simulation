89aadf92e939f1cb44fa45e5b2962af7c4d0d6ce
JoshHumpherey <jhumphrey321@gmail.com>
2018-03-15 13:16:54 -0400

    adding more work to p9

--- p9/p9.m
+++ p9/p9.m
@@ -1,6 +1,18 @@
 % System Simulation Problem 9
 % Written by Josh Humphrey
 
-syms s
-f = ((0.085)/(s^4+0.4174*s^3+1.0871*s^2+0.2805*s+0.1512));
-ztrans(f)
+N = 10000
+period = 10;
+z = linspace(0,period,N);
+f = @(z) 17/(200*((561*z)/2000 + (10871*(z - 1/z)^2)/10000 + (2087*(z - 1/z)^3)/5000 + (z - 1/z)^4 - 561/(2000*z) + 189/1250))
+results = linspace(0,period,N);
+% Plot Frequency Response of Fz
+for z = 1:N
+    results(z) = f(z);
+end
+
+plot(results)
+xlim([0,10])
+title('Frequency Response of F')
+
+
