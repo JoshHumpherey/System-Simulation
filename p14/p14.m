b030ab4c23dccdd7e367e82d58e9624bcc750ee7
JoshHumpherey <jhumphrey321@gmail.com>
2018-04-26 12:08:49 -0400

    Adding p14

--- p14/p14.m
+++ p14/p14.m
@@ -1,9 +1,26 @@
-% System Simulation Problem 14
-% Written by Josh Humphrey
+%Problem 14
+%System Simulation
 
+N = 10000;
+t = linspace(0,10,N);
+T_unstable = 2;
 a = 0.3;
 b = 0.2;
 c = 0.5;
 d = 0.03302;
-e = 0.01;
+E = 0.01;
+V = 0;
+I = zeros(1,N);
+Y = zeros(1,N);
+H = 100;
+
+for k = 1:N-3
+num = a*c*(V(k)-I(k))*(Y(k)/H)-d*I(k) == diff(I,k);
+den = a*b*I(k)*((H-Y(k))/H)-E*Y(k) == diff(Y,k);
+Solution = solve(den,Y);
+end
+
+figure(1)
+plot(t,ySol);
+title('Mosquito-Borne Disease')
 
--- p16/p16.m
+++ p16/p16.m
@@ -1,2 +1,25 @@
 % System Simulation Problem 16
 % Written by Josh Humphrey
+
+h=1;
+a = 0.07;
+b = 0.1;
+alpha = 2;
+beta = 1;
+x = 1:h:10;
+y = ones(1,length(x)); 
+y(1) = 1;
+F_xy = @(t,r) r;
+
+for k=1:(length(x)-1)
+    k_1 = F_xy(x(k),y(k));
+    k_2 = F_xy(x(k)+0.5*h,y(k)+0.5*h*k_1);
+    k_3 = F_xy((x(k)+0.5*h),(y(k)+0.5*h*k_2));
+    k_4 = F_xy((x(k)+h),(y(k)+k_3*h));
+
+    y(k+1) = y(k) + (1/6)*(k_1+2*k_2+2*k_3+k_4)*h;
+end
+
+figure(1)
+plot(x,y)
+title('Folded Torus System')
