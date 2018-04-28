6653823dbd4d350909a3a8a447370b3aa65e1b57
JoshHumpherey <jhumphrey321@gmail.com>
2018-04-28 11:18:22 -0400

    Adding p1 (completely wrong)

--- Final/p1/p1.m
+++ Final/p1/p1.m
@@ -1,4 +1,22 @@
 % System Simuation Final: Problem 1
 % Written by Josh Humphrey
+clear
+N = 10;
+T = 1;
+t = linspace(1,11,11);
+x = zeros(0,N);
+x(1) = 0;
+x(11) = 10;
+ffx = zeros(0,N);
 
+for m = 3:9
+    ffx(m) = -1+(1/(x(m)-x(m-1)).^2)-(1/(x(m+1)-x(m)).^2);
+    x(m) = (T^2)*ffx(m-1)-x(m-2)-2*x(m-1);
+    
+end
 
+figure(1)
+plot(t,x,'o')
+title('Magnets on a Frictionless Rod')
+xlabel('Time (seconds)')
+ylabel('Position')
--- Final/p3/p3.m
+++ Final/p3/p3.m
@@ -2,8 +2,8 @@
 % Written by Josh Humphrey
 
 % Model Parameters
-days = 180;
-T = 1;
+days = 400;
+T = 0.5;
 t = linspace(1,days,days);
 alpha = 0.005;
 beta = 0.0055;
@@ -26,7 +26,7 @@ I = zeros(1,days); % Infected Group
 Z = zeros(1,days); % Zombies
 R = zeros(1,days); % Raise-able
 K = zeros(1,days); % Unable to be re-animated
-S(1)= Columbus;
+S(1)= Cleveland;
 I(1)= 0;
 Z(1)= 0;
 R(1)= 0;
@@ -41,27 +41,51 @@ for k = 1:days-1
     RK1 = delta*S(k)-zeta*R(k);
     KK1 = delta*I(k)+alpha*S(k)*Z(k);
 
-    MidS = S(k)+(T)*SK1;
-    MidI = I(k)+(T)*IK1;
-    MidZ = Z(k)+(T)*ZK1;
-    MidR = R(k)+(T)*RK1;    
-    MidK = K(k)+(T)*KK1;
+    MidS1 = S(k)+(T/2)*SK1;
+    MidI1 = I(k)+(T/2)*IK1;
+    MidZ1 = Z(k)+(T/2)*ZK1;
+    MidR1 = R(k)+(T/2)*RK1;    
+    MidK1 = K(k)+(T/2)*KK1;
     
-    SK2 = p*MidS-beta*MidZ*MidS-delta*MidS+c*MidZ;
-    IK2 = beta*MidZ*MidS-rho*MidI-delta*MidI;
-    ZK2 = rho*MidI+zeta*MidR-alpha*MidS*MidZ-c*MidZ;
-    RK2 = delta*MidS-zeta*MidR;
-    KK2 = delta*MidI+alpha*MidS*MidZ;
+    SK2 = p*MidS1-beta*MidZ1*MidS1-delta*MidS1+c*MidZ1;
+    IK2 = beta*MidZ1*MidS1-rho*MidI1-delta*MidI1;
+    ZK2 = rho*MidI1+zeta*MidR1-alpha*MidS1*MidZ1-c*MidZ1;
+    RK2 = delta*MidS1-zeta*MidR1;
+    KK2 = delta*MidI1+alpha*MidS1*MidZ1;
 
-    S(k+1) = S(k)+(T/2)*(SK1 + SK2);
-    I(k+1) = I(k)+(T/2)*(IK1 + IK2);
-    Z(k+1) = Z(k)+(T/2)*(ZK1 + ZK2);
-    R(k+1) = R(k)+(T/2)*(RK1 + RK2);
-    K(k+1) = K(k)+(T/2)*(KK1 + KK2);
+    MidS2 = S(k)+(T/2)*SK2;
+    MidI2 = I(k)+(T/2)*IK2;
+    MidZ2 = Z(k)+(T/2)*ZK2;
+    MidR2 = R(k)+(T/2)*RK2;    
+    MidK2 = K(k)+(T/2)*KK2;
+    
+    SK3 = p*MidS2-beta*MidZ2*MidS2-delta*MidS2+c*MidZ2;
+    IK3 = beta*MidZ2*MidS2-rho*MidI2-delta*MidI2;
+    ZK3 = rho*MidI2+zeta*MidR2-alpha*MidS2*MidZ2-c*MidZ2;
+    RK3 = delta*MidS2-zeta*MidR2;
+    KK3 = delta*MidI2+alpha*MidS2*MidZ2;
+    
+    MidS3 = S(k)+(T)*SK3;
+    MidI3 = I(k)+(T)*IK3;
+    MidZ3 = Z(k)+(T)*ZK3;
+    MidR3 = R(k)+(T)*RK3;    
+    MidK3 = K(k)+(T)*KK3;
+    
+    SK4 = p*MidS3-beta*MidZ3*MidS3-delta*MidS3+c*MidZ3;
+    IK4 = beta*MidZ3*MidS3-rho*MidI3-delta*MidI3;
+    ZK4 = rho*MidI3+zeta*MidR3-alpha*MidS3*MidZ3-c*MidZ3;
+    RK4 = delta*MidS3-zeta*MidR3;
+    KK4 = delta*MidI3+alpha*MidS3*MidZ3;
+    
+    S(k+1) = S(k)+(T/6)*(SK1 + 2*SK2 + 2*SK3 + SK4);
+    I(k+1) = I(k)+(T/6)*(IK1 + 2*IK2 + 2*IK3 + IK4);
+    Z(k+1) = Z(k)+(T/6)*(ZK1 + 2*ZK2 + 2*ZK3 + ZK4);
+    R(k+1) = R(k)+(T/6)*(RK1 + 2*RK2 + 2*RK3 + RK4);
+    K(k+1) = K(k)+(T/6)*(KK1 + 2*KK2 + 2*KK3 + KK4);
 end
 
-Mid_Z = Z;
-Mid_H = S;
+Cle_Z = Z;
+Cle_H = S;
 
 figure(1)
 hold on
@@ -74,34 +98,34 @@ xlim([1,days])
 grid on
 xlabel('# of Days')
 ylabel('Population (Thousands)')
-title('Zombie Outbreak: Columbus')
+title('Zombie Outbreak: Cleveland')
 
-% figure(2)
-% hold on
-% grid on
-% plot(t,Mid_H)
-% plot(t,Akr_H)
-% plot(t,CF_H)
-% plot(t,Cle_H)
-% plot(t,Col_H)
-% title('Human Populations')
-% legend('Middletown','Akron','Canal Fulton','Cleveland','Columbus')
-% xlabel('# of Days')
-% ylabel('Population (Thousands)')
-% xlim([1,days])
-% hold off
-% 
-% figure(3)
-% hold on
-% grid on
-% plot(t,Mid_Z)
-% plot(t,Akr_Z)
-% plot(t,CF_Z)
-% plot(t,Cle_Z)
-% plot(t,Col_Z)
-% title('Zombie Populations')
-% legend('Middletown','Akron','Canal Fulton','Cleveland','Columbus')
-% xlabel('# of Days')
-% ylabel('Population (Thousands)')
-% xlim([1,days])
-% hold off
+figure(2)
+hold on
+grid on
+plot(t,Mid_H)
+plot(t,Akr_H)
+plot(t,CF_H)
+plot(t,Cle_H)
+plot(t,Col_H)
+title('Human Populations')
+legend('Middletown','Akron','Canal Fulton','Cleveland','Columbus')
+xlabel('# of Days')
+ylabel('Population (Thousands)')
+xlim([1,days])
+hold off
+
+figure(3)
+hold on
+grid on
+plot(t,Mid_Z)
+plot(t,Akr_Z)
+plot(t,CF_Z)
+plot(t,Cle_Z)
+plot(t,Col_Z)
+title('Zombie Populations')
+legend('Middletown','Akron','Canal Fulton','Cleveland','Columbus')
+xlabel('# of Days')
+ylabel('Population (Thousands)')
+xlim([1,days])
+hold off
