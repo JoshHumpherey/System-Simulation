82afc784343314664283e370da6572685abd30f7
Joshua Humphrey <JCHumphrey@drbsystems.com>
2018-04-30 10:41:11 -0400

    Small changes to P1

--- Final/p1/p1.m
+++ Final/p1/p1.m
@@ -1,22 +1,23 @@
 % System Simuation Final: Problem 1
 % Written by Josh Humphrey
 clear
-N = 10;
+N = 11;
 T = 1;
-t = linspace(1,11,11);
-x = zeros(0,N);
+t = linspace(1,N,N);
+x = ones(1,N);
 x(1) = 0;
 x(11) = 10;
-ffx = zeros(0,N);
-
+ffx = zeros(1,N);
+figure(1)
+hold on
+grid on
 for m = 3:9
-    ffx(m) = -1+(1/(x(m)-x(m-1)).^2)-(1/(x(m+1)-x(m)).^2);
+    ffx(m) = -1+(1/((x(m)-x(m-1)).^2))-(1/((x(m+1)-x(m)).^2));
     x(m) = (T^2)*ffx(m-1)-x(m-2)-2*x(m-1);
-    
+    plot(t,x(m),'o')
 end
 
-figure(1)
-plot(t,x,'o')
 title('Magnets on a Frictionless Rod')
 xlabel('Time (seconds)')
 ylabel('Position')
+hold off
