f62fdf7dbc8befa7d2461678fc4f055e637c6167
JoshHumpherey <jhumphrey321@gmail.com>
2018-05-06 13:54:22 -0400

    Final System Sim Commit :)

--- Final/p4/p4.m
+++ Final/p4/p4.m
@@ -1,56 +1,109 @@
 % System Simuation Final: Problem 4
 % Written by Josh Humphrey
+%% Part E:
 clear
-N = 100000;
+Nr=10;
+Nt=21;
+animate=0;
+rvec=linspace(1,Nr,Nr)/Nr;
+
+theta=linspace(-pi,pi,1001);
+theta=theta(1:length(theta)-1);
+z=1*exp(1i*theta);
+
+b0 = (6/17);
+b1 = (11/17);
+b2 = (1);
+b3 = (1);
+xlow = -10;
+xhigh = 0;
+ylow = -6;
+yhigh = 6;
+figure(1)
+for m=1:Nr
+    z=rvec(Nr-m+1)*exp(1i*theta);
+    w = (z.^2-z)./(b3*z.^3+b2*z.^2+b1*z+b0);
+    hold on
+    plot(real(w),imag(w))
+    hold off
+    title(['Part E: Stability Region; r=',num2str(rvec(m))])
+    xlabel('Re\{{\lambda}T \}')
+    ylabel('Im\{{\lambda}T \}')
+    axis([xlow xhigh ylow yhigh])
+    axis square
+    grid on
+    pause(0.5)
+end
+
+tvec=linspace(0,Nt-1,Nt)*(2*pi)/Nt;
+rho=linspace(0,1,101);
+z=1*exp(1i*theta);
+    w = (z.^2-z)./(b3*z.^3+b2*z.^2+b1*z+b0);
+hold on
+    plot(real(w),imag(w))
+hold off
+
+axis([xlow xhigh ylow yhigh])
+axis square
+grid on
+
+
+for m=1:Nt
+    z=rho*exp(1i*tvec(m));
+    w = (z.^2-z)./(b3*z.^3+b2*z.^2+b1*z+b0);
+
+    if(animate==1)
+        for n=1:length(rho)
+            hold on
+                plot(real(w(n)),imag(w(n)))
+            hold off
+            pause(0.01)
+        end
+    end
+
+    hold on
+        plot(real(w),imag(w))
+    hold off
+    title('Part E: Stability Region')
+    xlabel('Re\{{\lambda}T \}')
+    ylabel('Im\{{\lambda}T \}')
+    axis([xlow xhigh ylow yhigh])
+    axis square
+    grid on
+end
+
+
+
+%% Part F:
+N = 10000;
 T = 0.01;
 t = linspace(0,1000,N);
 x1 = zeros(1,N);
 x2 = zeros(1,N);
 x3 = zeros(1,N);
-
-x1(1) = 0.1;
-x2(1) = 0.1;
-x3(1) = 0.1;
+f1 = zeros(1,N);
+f2 = zeros(1,N);
+f3 = zeros(1,N);
+x1(1) = 1;
+x2(1) = 1*10^10;
+x3(1) = 1*10^-10;
 
 for k = 1:(N-1)
-    X1_K1 = -0.04*x1(k)+(10^4)*x2(k)*x3(k);
-    X2_K1 = 0.04*x1(k)-(10^4)*x2(k)*x3(k)-(3*10^7)*(x2(k))^2;
-    X3_K1 = (3*10^7)*(x2(k))^2;
+    f1(k) = -0.04*x1(k)+(10^4)*x2(k)*x3(k);
+    x1(k+1) = x1(k)+T*f1(k);
     
-    MidX1_1 = x1(k)+(T/2)*X1_K1;
-    MidX2_1 = x2(k)+(T/2)*X2_K1;
-    MidX3_1 = x3(k)+(T/2)*X3_K1;
+    f2(k) = 0.04*x1(k)-(10^4)*x2(k)*x3(k)-(3*10^7)*(x2(k))^2;
+    x2(k+1) = x2(k)+T*f2(k);
     
-    X1_K2 = -0.04*MidX1_1+(10^4)*MidX2_1*MidX3_1;
-    X2_K2 = 0.04*MidX1_1-(10^4)*MidX2_1*MidX3_1-(3*10^7)*(MidX2_1)^2;
-    X3_K2 = (3*10^7)*(MidX2_1)^2;
-    
-    MidX1_2 = x1(k)+(T/2)*X1_K2;
-    MidX2_2 = x2(k)+(T/2)*X2_K2;
-    MidX3_2 = x3(k)+(T/2)*X3_K2;
-    
-    X1_K3 = -0.04*MidX1_2+(10^4)*MidX2_2*MidX3_2;
-    X2_K3 = 0.04*MidX1_2-(10^4)*MidX2_2*MidX3_2-(3*10^7)*(MidX2_2)^2;
-    X3_K3 = (3*10^7)*(MidX2_2)^2;
-    
-    MidX1_3 = x1(k)+(T)*X1_K3;
-    MidX2_3 = x2(k)+(T)*X2_K3;
-    MidX3_3 = x3(k)+(T)*X3_K3;
-    
-    X1_K4 = -0.04*MidX1_3+(10^4)*MidX2_3*MidX3_3;
-    X2_K4 = 0.04*MidX1_3-(10^4)*MidX2_3*MidX3_3-(3*10^7)*(MidX2_3)^2;
-    X3_K4 = (3*10^7)*(MidX2_3)^2;
-    
-    x1(k+1) = x1(k)+(T/6)*(X1_K1 + 2*X1_K2 + 2*X1_K3 + X1_K4);
-    x2(k+1) = x2(k)+(T/6)*(X2_K1 + 2*X2_K2 + 2*X2_K3 + X2_K4);
-    x3(k+1) = x3(k)+(T/6)*(X3_K1 + 2*X3_K2 + 2*X3_K3 + X3_K4);
+    f3(k) = (3*10^7)*(x2(k))^2;
+    x3(k+1) = x3(k)+T*f3(k);
 end
 
-figure(1)
+figure(2)
 grid on
 hold on
-plot(t,x1)
-plot(t,x2)
-plot(t,x3)
+semilogx(t,x1)
+semilogx(t,x2)
+semilogx(t,x3)
 title('Problem 4')
 legend('x1','x2','x3')
